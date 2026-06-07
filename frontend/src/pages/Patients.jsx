012c53b80ad0e98433abd0976e5deebb25935efe
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2026-06-07 11:29:24 -0700

    Add Patients component with patient table

--- frontend/src/pages/Patients.jsx
+++ frontend/src/pages/Patients.jsx
@@ -1,1 +1,29 @@
+export default function Patients() {
+  return (
+    <div>
+      <h1>Patients</h1>
 
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Patient No</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>P001</td>
+            <td>John</td>
+            <td>Doe</td>
+            <td>Male</td>
+            <td>1990-01-01</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
