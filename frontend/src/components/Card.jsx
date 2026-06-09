65670c13e3b3424e5d419e13d1e4593d29d842fe
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2026-06-09 06:29:26 -0700

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,1 +1,22 @@
+export default function Card({title,value}){
 
+return (
+
+<div style={{
+background:"white",
+padding:"20px",
+borderRadius:"12px",
+boxShadow:"0 2px 8px #ddd"
+}}>
+
+
+<h3>{title}</h3>
+
+<h1>{value}</h1>
+
+
+</div>
+
+)
+
+}
