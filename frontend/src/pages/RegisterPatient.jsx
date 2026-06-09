1080f94fe30f83925f11c55dcf244d642c5f28c0
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2026-06-09 06:56:09 -0700

    Create RegisterPatient.jsx

---
+++ frontend/src/pages/RegisterPatient.jsx
@@ -0,0 +1,64 @@
+export default function RegisterPatient(){
+
+return (
+
+<div>
+
+<h1>
+Register Patient
+</h1>
+
+
+<form>
+
+
+<input placeholder="First Name"/>
+
+<br/><br/>
+
+<input placeholder="Last Name"/>
+
+<br/><br/>
+
+
+<select>
+
+<option>
+Male
+</option>
+
+<option>
+Female
+</option>
+
+</select>
+
+
+<br/><br/>
+
+
+<input type="date"/>
+
+
+<br/><br/>
+
+
+<input placeholder="Phone"/>
+
+
+<br/><br/>
+
+
+<button>
+Save Patient
+</button>
+
+
+</form>
+
+
+</div>
+
+)
+
+}
