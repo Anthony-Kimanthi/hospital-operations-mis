769dddc0ca29c3a449d2b56c364fa0689b4d0443
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2026-06-09 06:52:01 -0700

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,1 +1,31 @@
+export default function Header(){
 
+return (
+
+<header
+style={{
+height:"70px",
+background:"#ffffff",
+display:"flex",
+alignItems:"center",
+justifyContent:"space-between",
+padding:"0 25px",
+borderBottom:"1px solid #ddd"
+}}
+>
+
+<h2>
+Hospital Operations MIS
+</h2>
+
+
+<div>
+Admin
+</div>
+
+
+</header>
+
+)
+
+}
