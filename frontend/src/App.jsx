cdeb187dc9aafc1c5d6053492573cd01a452a3c5
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2026-08-24 12:30:54 -0700

    Refactor App component to use BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,37 @@
-import {
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./layouts/Layout";
 
 import Dashboard from "./pages/Dashboard";
 import Patients from "./pages/Patients";
+import RegisterPatient from "./pages/RegisterPatient";
+import PatientProfile from "./pages/PatientProfile";
 import Staff from "./pages/Staff";
 import Visits from "./pages/Visits";
-import RegisterPatient from "./pages/RegisterPatient";
 
-export default function App(){
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
 
-return (
+          <Route path="/patients" element={<Patients />} />
+          <Route
+            path="/patients/register"
+            element={<RegisterPatient />}
+          />
+          <Route
+            path="/patients/:patientId"
+            element={<PatientProfile />}
+          />
 
-<Routes>
-
-<Route element={<Layout/>}>
-
-
-<Route 
-path="/" 
-element={<Dashboard/>}
-/>
-
-
-<Route
-path="/patients"
-element={<Patients/>}
-/>
-
-
-<Route
-path="/staff"
-element={<Staff/>}
-/>
-
-
-<Route
-path="/visits"
-element={<Visits/>}
-/>
-<Route
-path="/patients/register"
-element={<RegisterPatient/>}
-/>
-
-</Route>
-
-</Routes>
-
-)
-
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/visits" element={<Visits />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
